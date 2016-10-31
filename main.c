adde6c296c65013b42ef350ed53e6980ac11bc48
Alex Yatskov <alex@foosoft.net>
2016-10-31 09:04:15 -0700

    WIP

--- main.c
+++ main.c
@@ -1,3 +1,21 @@
+/*
+ * Copyright (C) 2016  Alex Yatskov <alex@foosoft.net>
+ * Author: Alex Yatskov <alex@foosoft.net>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <stdio.h>
 #include <iconv.h>
 #include <string.h>
@@ -97,7 +115,8 @@ static void dump_hits(EB_Book* book) {
             eb_seek_text(book, &hits[i].text);
             eb_read_text(book, NULL, NULL, NULL, MAX_ENTRY_TEXT, text, &text_length);
 
-            puts(eucjp_to_utf8(text));
+            const char* text_utf8 = eucjp_to_utf8(text);
+            (void) text_utf8;
         }
     }
     while (hit_count > 0);
